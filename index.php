8455c31801998b0a71b47353c141b614e0df6680
thiemsothypanha-coder <thiemsothypanha@gmail.com>
2025-12-31 19:47:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+$Sticker3500 = null;
+
+$StickerBuss = null;
+$StickerPlastic = null;
+$PhotoCard = null;
+$InstaxPhoto = null;
+$PhotoBooth = null;
+$keychain = null;
+$Invoice = null;
+$Books = null;
+$CoverBooks = null;
+$PhotoAlabume = null;
+$Photo13×18 = null;
+$InvoiceBuss = null;
+$NameCard = null;
+$
+$Delivery = null;
+
+echo "<h1>RS Printing</h1>";
+echo "<h3>INVOICE</h3>";
+echo "<h4>Sticker: <h4>";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello: <?php $name?></h1>
+    <label for="name">Customer Name</label>
+    <input id="name" type="text">
+    <button id="enter">Enter</button>
+    
+</body>
+</html>
+<script>
+    $()
+</script>
+
